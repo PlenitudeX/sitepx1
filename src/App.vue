487f768bce99d8627347a7f2af63e7d6a1be1438
Pablo Lima <pablo.oliveira77@outlook.com.br>
2023-09-19 17:32:27 -0300

    PabloSite

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue';
 import Banner from './components/Banner.vue';
 </script>
-
+  
 <template>
     <Header />
     <Banner />
